de5140abd00746b1a5efcf6ebfdd8dfe48c30871
tgujar <tanmaygujar999@gmail.com>
2023-01-04 18:34:18 -0800

    added user model and register route

---
+++ backend/controllers/userController.js
@@ -0,0 +1,64 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = reqire('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+// @desc Register user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler (async (req, res) => {
+  const {name, email, password} = req.body
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+  // Check if user exists
+  const userExists = await User.findOne({email})
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPass = await bcrypt.hash(password, salt)
+
+  // Create User
+  const user = await User.create({
+    name, 
+    email,
+    password: hashedPass
+  })
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error ("Invalid user details")
+  } 
+});
+
+// @desc Authenticate a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler (async (req, res) => {
+  res.json({ message: "Login user" });
+});
+
+// @desc Get user data
+// @route GET /api/users/me
+// @access Private
+const getMe = asyncHandler (async (req, res) => {
+  res.json({ message: "User data display" });
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exporst = mongoose.model("User", userSchema);
